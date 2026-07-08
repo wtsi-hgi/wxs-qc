e1a2edf5f83633a4d4d57bd92c05ad0334ba681a
Gennadii Zakharov <gz3@sanger.ac.uk>
2026-07-08 09:15:16 +0100

    Additional checkpoints for step 2.2.

--- 2-sample_qc/2-prune_related_samples.py
+++ 2-sample_qc/2-prune_related_samples.py
@@ -10,6 +10,7 @@ from wes_qc.compute_relatedness import prune_pc_relate
 from wes_qc.filtering import filter_matrix_for_ldprune
 from wes_qc import hail_utils, constants, filtering
 
+
 def plot_relatedness(
     relatedness_ht: hl.Table, relatedness_plotfile: str, text_size=constants.plots_text_size, **kwargs
 ) -> None:
@@ -25,12 +26,13 @@ def plot_relatedness(
     bkplt.output_file(relatedness_plotfile)
     bkplt.save(layout)
 
+
 # TODO: How is this step different from 2-sample_qc/3-population_pca_prediction.py/run_pca()? Only PCA plotting?
 def run_population_pca(
     filtered_mt: hl.MatrixTable,
     samples_to_remove: hl.Table,
     prune_args: dict,
-#    plink_outfile,
+    #    plink_outfile,
     pca_components,
     plot_outfile,
     pruned_unrelated_pcrelate_file: str,
@@ -44,20 +46,20 @@ def run_population_pca(
     print("=== Spliting samples")
     unrelated_mt = filtered_mt.filter_cols(hl.is_defined(samples_to_remove[filtered_mt.col_key]), keep=False)
     related_mt = filtered_mt.filter_cols(hl.is_defined(samples_to_remove[filtered_mt.col_key]))
-    variants, samples = unrelated_mt.count()
+    samples = unrelated_mt.count_cols()
     print(f"=== {samples} samples for PCA")
-    variants, samples = related_mt.count()
+    samples = related_mt.count_cols()
     print(f"=== {samples} samples for PC projection")
-    #Why do we need plink files?
-    #plink_mt = unrelated_mt.annotate_cols(uid=unrelated_mt.s).key_cols_by("uid")
-    #hl.export_plink(dataset=plink_mt, output=path_spark(plink_outfile), fam_id=plink_mt.uid, ind_id=plink_mt.uid)
+    # Why do we need plink files?
+    # plink_mt = unrelated_mt.annotate_cols(uid=unrelated_mt.s).key_cols_by("uid")
+    # hl.export_plink(dataset=plink_mt, output=path_spark(plink_outfile), fam_id=plink_mt.uid, ind_id=plink_mt.uid)
     print("=== Running LD pruning before PCA")
     unrelated_mt = prune_mt(unrelated_mt, prune_args["ld_prune_args"])
-    unrelated_mt=unrelated_mt.checkpoint(path_spark(pruned_unrelated_pcrelate_file), overwrite=True)
+    unrelated_mt = unrelated_mt.checkpoint(path_spark(pruned_unrelated_pcrelate_file), overwrite=True)
     related_mt = related_mt.semi_join_rows(unrelated_mt.rows())
 
-    union_pca_scores, pca_scores, pca_loadings, _ =run_pc_project(unrelated_mt, related_mt, pca_components)
-    
+    union_pca_scores, pca_scores, pca_loadings, _ = run_pc_project(unrelated_mt, related_mt, pca_components)
+
     pca_mt = filtered_mt.annotate_cols(scores=union_pca_scores[filtered_mt.col_key].scores)
     print("=== Plotting PC1 vs PC2")
     os.makedirs(os.path.dirname(plot_outfile), exist_ok=True)
@@ -67,19 +69,17 @@ def run_population_pca(
     bkplt.save(p)
     return pca_mt, union_pca_scores, pca_scores, pca_loadings
 
+
 def main():
     # = STEP SETUP = #
     config = parse_config()
     tmp_dir = config["general"]["tmp_dir"]
 
     # = STEP PARAMETERS = #
-    control_list=config["general"]["metadata"]["control_samples"]
+    control_list = config["general"]["metadata"]["control_samples"]
 
     # = STEP DEPENDENCIES = #
     mt_infile = config["step2"]["impute_sex"]["sex_mt_outfile"]
-
-    # = STEP OUTPUTS = #
-    mtoutfile = config["step2"]["prune_params"]["pruned_mt_file"]
 
     # = STEP LOGIC = #
     _ = hail_utils.init_hl(tmp_dir)
@@ -90,18 +90,24 @@ def main():
         os.makedirs(pltdir)
     # load input mt
     mt = hl.read_matrix_table(path_spark(mt_infile))
-    #removing control samples
-    mt= filtering.remove_samples(mt, control_list)
-    #filter matrix to have good variants
-    filtered_mt = filter_matrix_for_ldprune(mt, config["step2"]["long_range_ld_file"], **config["step2"]["filter_params"])
-    filtered_mt.write(path_spark(config["step2"]["filtered_mt_outfile"]), overwrite=True)
+    # removing control samples
+    mt = filtering.remove_samples(mt, control_list)
+    # filter matrix to have good variants
+    filtered_mt = filter_matrix_for_ldprune(
+        mt, config["step2"]["long_range_ld_file"], **config["step2"]["filter_params"]
+    )
+    filtered_mt = filtered_mt.checkpoint(path_spark(config["step2"]["filtered_mt_outfile"]), overwrite=True)
 
     # run pcrelate
     related_samples_to_remove_ht, relatedness_ht = prune_pc_relate(
-        filtered_mt, config["step2"]["prune_params"], config["step2"]["king_params"], config["step2"]["pc_relate_params"], "study"
+        filtered_mt,
+        config["step2"]["prune_params"],
+        config["step2"]["king_params"],
+        config["step2"]["pc_relate_params"],
+        "study",
     )
-    
-    related_samples_to_remove_ht.write(
+
+    related_samples_to_remove_ht = related_samples_to_remove_ht.checkpoint(
         path_spark(config["step2"]["relatedness_output"]["samples_to_remove_file"]), overwrite=True
     )
     related_samples_to_remove_ht.export(path_spark(config["step2"]["relatedness_output"]["samples_to_remove_tsv"]))
